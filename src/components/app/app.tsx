05fa416af138cb1ce31de584d7b37880bfb3a2e4
Albina S <albina.sarkitova@yandex.ru>
2026-05-14 08:19:03 +0300

    Проектная работа 10

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,26 +1,53 @@
-import { CSSProperties } from 'react';
+import { CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from '../article/Article';
 import { ArticleParamsForm } from '../article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './../../constants/articleProps';
+import {
+	defaultArticleState,
+	ArticleStateType,
+} from './../../constants/articleProps';
 
 import styles from './app.module.scss';
 
 export const App = () => {
+	// 1. Глобальное состояние страницы
+	const [articleState, setArticleState] =
+		useState<ArticleStateType>(defaultArticleState);
+
+	// 2. Состояние открытия/закрытия панели
+	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+	// Обработчик применения настроек из формы
+	const handleApplyParams = (params: ArticleStateType) => {
+		setArticleState(params);
+		setIsSidebarOpen(false); // Закрываем панель после применения
+	};
+
+	// Обработчик сброса настроек
+	const handleResetParams = () => {
+		setArticleState(defaultArticleState);
+	};
+
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': articleState.fontFamilyOption.value,
+					'--font-size': articleState.fontSizeOption.value,
+					'--font-color': articleState.fontColor.value,
+					'--container-width': articleState.contentWidth.value,
+					'--bg-color': articleState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm
+				isOpen={isSidebarOpen}
+				onToggle={setIsSidebarOpen}
+				currentState={articleState}
+				onApply={handleApplyParams}
+				onReset={handleResetParams}
+			/>
 			<Article />
 		</main>
 	);
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,18 +1,125 @@
+import { SyntheticEvent, useState } from 'react';
+
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { Select } from 'src/ui/select';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
 
 import styles from './ArticleParamsForm.module.scss';
+import {
+	ArticleStateType,
+	backgroundColors,
+	contentWidthArr,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+	defaultArticleState,
+} from 'src/constants/articleProps';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	isOpen: boolean;
+	onToggle: (value: boolean) => void;
+	currentState: ArticleStateType;
+	onApply: (params: ArticleStateType) => void;
+	onReset: () => void;
+};
+
+export const ArticleParamsForm = ({
+	isOpen,
+	onToggle,
+	currentState,
+	onApply,
+	onReset,
+}: ArticleParamsFormProps) => {
+	// Состояние формы
+	const [formState, setFormState] = useState<ArticleStateType>(currentState);
+
+	// Обработчик сброса
+	const handleReset = () => {
+		setFormState(defaultArticleState);
+		onReset();
+	};
+
+	// Обработчик применения
+	const handleSubmit = (e: SyntheticEvent) => {
+		e.preventDefault();
+		onApply(formState);
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton isOpen={isOpen} onClick={() => onToggle(!isOpen)} />
+			<aside
+				className={`${styles.container} ${
+					isOpen ? styles.container_open : ''
+				}`}>
+				<form className={styles.form} onSubmit={handleSubmit}>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={handleReset}
+						/>
 						<Button title='Применить' htmlType='submit' type='apply' />
 					</div>
+
+					{/*НАЧАЛО НАПОЛНЕНИЯ ФОРМЫ*/}
+					{/* Выбор шрифта */}
+					<Select
+						selected={formState.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={(option) =>
+							setFormState((prev) => ({ ...prev, fontFamilyOption: option }))
+						}
+						title='Шрифт'
+					/>
+
+					{/* Размер шрифта */}
+					<RadioGroup
+						name='font-size'
+						options={fontSizeOptions}
+						selected={formState.fontSizeOption}
+						onChange={(option) =>
+							setFormState((prev) => ({ ...prev, fontSizeOption: option }))
+						}
+						title='Размер шрифта'
+					/>
+
+					<Separator />
+
+					{/* Цвет шрифта */}
+					<Select
+						selected={formState.fontColor}
+						options={fontColors}
+						onChange={(option) =>
+							setFormState((prev) => ({ ...prev, fontColor: option }))
+						}
+						title='Цвет шрифта'
+					/>
+
+					<Separator />
+
+					{/* Цвет фона */}
+					<Select
+						selected={formState.backgroundColor}
+						options={backgroundColors}
+						onChange={(option) =>
+							setFormState((prev) => ({ ...prev, backgroundColor: option }))
+						}
+						title='Цвет фона'
+					/>
+
+					{/* Ширина контента */}
+					<Select
+						selected={formState.contentWidth}
+						options={contentWidthArr}
+						onChange={(option) =>
+							setFormState((prev) => ({ ...prev, contentWidth: option }))
+						}
+						title='Ширина контента'
+					/>
 				</form>
 			</aside>
 		</>
